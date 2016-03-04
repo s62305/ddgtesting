38e1c4022e215d91e080f956ec6c4ad9f57a974b
Elena <elena_kireeva@ymail.com>
2016-03-04 23:57:46 +0300

    1. Add new case 'zodiac' and test it. 2. Rewritten a testing function. Several functions for testing different cases are replaced by one. 3. Early after executing the function "checkType" test wrote "error" but not failed. Now it failed.

--- test_ia.py
+++ test_ia.py
@@ -12,8 +12,8 @@ cases = {
     "calc": [
         ("1 + 2", "3"),
         ("9*4", "36"),
-        ("4/5", "0.8"),
-        ("for_error", "0.8"),
+        ("4/5", "0.8")
+        #("for_error", "0.8")
     ],
 
     "currency_in": [
@@ -26,9 +26,15 @@ cases = {
         ("12:00 UTC+7 to MSK", "20:00 MSK (1 day prior)"),
         ("10:00 MSK to CET", "8:00 CET"),
         ("20:00 MSK to UTC", "17:00 UTC")
-    ]    
+    ],
+
+    "zodiac": [
+        ("zodiac 21st June", "Gemini"),
+        ("zodiac 1st July", "Cancer"),
+        ("zodiac 29st August", "Virgo")
+    ]
 }    
-    
+
 def makeRequest(query, params = dict()):
     with pytest.allure.step("Make a request: q={}".format(query)):
 	params.update(q = query)
@@ -39,44 +45,43 @@ def checkType(ans_type, expected_type, query):
         if(ans_type == ""):
             ans_type = "type not found"
         print "Error 1 ( not ", expected_type, ")  : query = ", query, ", ans_type = ", ans_type
-        return 0
-    return 1
+        
+def getDDGValue(ans_type, parsed_json):
+    ddg_value = ""
+
+    if(ans_type == "calc"):
+        ddg_value = BeautifulSoup(parsed_json['Answer']).find('a').getText()
+    else:
+        if(ans_type == "zodiac"):
+            ddg_value = parsed_json['Answer']['data']['title']
+        else:
+            ddg_value = parsed_json['Answer']
+    
+    return ddg_value
     
 def checker(query, expected_value, expected_ans_type):
     params = DEFAULTS.copy()
     req = makeRequest(query, params)
-    
     parsed_json = json.loads(req.text)
+    
     # check "AnswerType"
     ans_type = BeautifulSoup(parsed_json['AnswerType']).getText()
-    ct = checkType(ans_type, expected_ans_type, query)
-    if(ct == 0):
-        return
+    checkType(ans_type, expected_ans_type, query)
     
     # check "Answer"
-    if(ans_type == "calc"):
-        ddg_value = BeautifulSoup(parsed_json['Answer']).find('a').getText()
-    else:
-        ddg_value = parsed_json['Answer']
+    ddg_value = getDDGValue(ans_type, parsed_json)
     
     with pytest.allure.step("Compare the values"):
         assert ddg_value == expected_value
     
-params = cases["calc"]
-@pytest.mark.parametrize("test_input, expected", params, ids = params)
-def test_calc(test_input, expected):
-    checker(test_input, expected, "calc")
-         
-params = cases["currency_in"]
-@pytest.mark.parametrize("test_input, expected", params, ids = params)
-def test_currency_in(test_input, expected):
-    checker(test_input, expected, "currency_in")
-                
-params = cases["timezone_converter"]
-@pytest.mark.parametrize("test_input, expected", params, ids = params)
-def test_timezone_converter(test_input, expected):
-    checker(test_input, expected, "timezone_converter")
-        
-    
+def getParams():
+    params_list = []
+    for c in cases.items():
+        for i in range(0, len(c[1])):
+            params_list.append((c[0], c[1][i][0], c[1][i][1]))
+    return params_list        
 
+@pytest.mark.parametrize("answer_type, test_input, expected", getParams())
+def tests(answer_type, test_input, expected):
+    checker(test_input, expected, answer_type)
     
