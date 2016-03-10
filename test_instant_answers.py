d1f51f48a79f6cdadbd9f60d6871280e283726d8
Литус Ярослав <litus@corp.sputnik.ru>
2016-03-10 10:30:01 +0300

    (cosmetic): minor fixes debug prints style naming

--- test_instant_answers.py
+++ test_instant_answers.py
@@ -11,8 +11,7 @@ cases = {
         ("1 + 2", "3"),
         ("9*4", "36"),
         ("4/5", "0.8")
-        #("for_error", "0.8")
-        ],
+    ],
 
     "currency_in": [
         ("currency in US", "United States Dollar (USD)"),
@@ -24,14 +23,14 @@ cases = {
         ("12:00 UTC+7 to MSK", "20:00 MSK (1 day prior)"),
         ("10:00 MSK to CET", "8:00 CET"),
         ("20:00 MSK to UTC", "17:00 UTC")
-        ],
+    ],
 
     "zodiac": [
         ("zodiac 21st June", "Gemini"),
         ("zodiac 1st July", "Cancer"),
         ("zodiac 29st August", "Virgo")
-        ]
-}    
+    ]
+}
 
 def getDDGValue(ans_type, parsed_json):
     ddg_value = parsed_json['Answer']
@@ -43,24 +42,30 @@ def getDDGValue(ans_type, parsed_json):
         
     return ddg_value
 
-params = [(k,) + item for k, v in cases.items() for item in v]
 
-@pytest.mark.parametrize("expected_ans_type, query, expected_value", params)
-def tests_instant_answer(expected_ans_type, query, expected_value):
+params = [(ans_type,) + case for ans_type, case_list in cases.items() for case in case_list]
+idlist = ["({}) q = {!r}".format(case[0], case[1]) for case in params]
 
+@pytest.mark.parametrize("expected_ans_type, query, expected_value", params, ids = idlist)
+def test_instant_answer(expected_ans_type, query, expected_value):
     with pytest.allure.step("Make a request: q={}".format(query)):
         params = DEFAULTS.copy()
         params.update(q = query)
         req = requests.get(HOST, params = params)
         print req.url
-        
+
+        req.raise_for_status()
+
     with pytest.allure.step("Check answer type"):
         parsed_json = json.loads(req.text)
         ans_type = BeautifulSoup(parsed_json['AnswerType']).getText()
+        print "AnswerType: {!r}".format(ans_type)
+
         assert ans_type == expected_ans_type
-        
-    ddg_value = getDDGValue(ans_type, parsed_json)
-    
-    with pytest.allure.step("Compare the values"):
+
+    with pytest.allure.step("Expecting answer: {!r}".format(expected_value)):
+        ddg_value = getDDGValue(ans_type, parsed_json)
+        print "Answer: {!r}".format(parsed_json["Answer"])
+        print "Answer (reduced): {!r}".format(ddg_value)
+
         assert ddg_value == expected_value
-    
